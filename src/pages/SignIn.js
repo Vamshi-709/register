8078fa1a6b4853dd2bb8f0fa718aa18a16720b8a
Vamshi <gvamshi906@gmail.com>
2022-06-20 12:54:10 +0530

    completed

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -8,11 +8,11 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 
-
 const SignIn = (callback, validate) => {
 
   const navigate = useNavigate()
   const [values, setValues] = useState()
+
 
   const handleChnage = (e) => {
     setValues(e.target.value)
@@ -21,10 +21,10 @@ const SignIn = (callback, validate) => {
   return (
     <div>
       <h2>Add  your personal details</h2>
-      <p>Loerm ipsum is simply dummy text of the printing and typesetting industry. </p><br /><br />
-      <form className='form'>
+      <p>Loerm ipsum is simply dummy text of the printing and typesetting industry. </p><br />
+      <form className='form   needs-validation'>
         <label className='form-data1' name="" onChange={handleChnage}>Full Name</label><br />
-        <input className='input' type="text" /><br /><br />
+        <input className='input' type="text" id="validationCustom02" /><br /><br />
 
         <FormControl>
           <FormLabel id="demo-row-radio-buttons-group-label" className="form-data3">Gender</FormLabel>
@@ -39,16 +39,26 @@ const SignIn = (callback, validate) => {
           </RadioGroup>
         </FormControl><br />
 
-        <label className='form-data2' >Country</label><br />
-        <input className='input' type="text" /><br /><br />
+        <label className='form-data' onChange={handleChnage} >Country</label><br />
+        <select className='input-1'>
+          <option selected >India</option>
+          <option value="1">Australia</option>
+          <option value="2">USA</option>
+          <option value="3">Canada</option>
+        </select><br /><br />
 
         <label className='form-data' onChange={handleChnage}>State</label><br />
-        <input className='input' type="text" /><br /><br />
+        <select className='input-1'>
+          <option selected >Telangana</option>
+          <option value="1">Andrapradhesh</option>
+          <option value="2">Karnataka</option>
+          <option value="3">kerala</option>
+        </select><br /><br />
 
         <label className='form-data' oonChange={handleChnage}>Phone</label><br />
         <input type="number" className='input' /><br /><br />
 
-        <button className='form-input-btn' onClick={() => { navigate("/main") }}>Next</button>
+        <button type="submit" className='form-input-btn' onClick={() => { navigate("/main") }}>Next</button>
       </form>
       <p>Alredy have  an account ? <a href='login'>Log in</a></p>
 
--- src/pages/VerifyOTP.js
+++ src/pages/VerifyOTP.js
@@ -6,19 +6,54 @@ import './VerifyOTP.css'
 function VerifyOTP() {
     let navigate = useNavigate()
 
-    return (
-        <div>
-            <h2>Enter your OTP</h2>
-            <p> For your security,we need to verify your identity.We sent a 5- digit code to<br />
-                { }.Please enter it Below.</p><br />
-            <div style={{ backgroundColor: "azure" }}>
 
-                <label className='form-data' >Enter your Code</label>
-                <div className='contant' >
-                    <div class="d-flex flex-row mt-5" ><input type="text" class="form-control" autofocus="" /><input type="text" class="form-control" /><input type="text" class="form-control" /><input type="text" class="form-control" /><input type="text" class="form-control" /></div>
-                </div><br />
-                <button class="btn" onClick={() => navigate('/main')}>Back</button> <button className='form-input-btn'>Verify</button>
-                <p>Did't receive the email ? Check  your spam filter for an email from { }</p>
+    document.addEventListener("DOMContentLoaded", function (event) {
+
+        function OTPInput() {
+            const inputs = document.querySelectorAll('#otp > *[id]');
+            for (let i = 0; i < inputs.length; i++) {
+                inputs[i].addEventListener('keydown', function (event) {
+                    if (event.key === "Backspace") {
+                        inputs[i].value = ''; if (i !== 0) inputs[i - 1].focus();
+                    } else {
+                        if (i === inputs.length - 1 && inputs[i].value !== '') {
+                            return true;
+                        } else if (event.keyCode > 47 && event.keyCode < 58) {
+                            inputs[i].value = event.key;
+                            if (i !== inputs.length - 1) inputs[i + 1].focus(); event.preventDefault();
+                        } else if (event.keyCode > 64 && event.keyCode < 91) {
+                            inputs[i].value = String.fromCharCode(event.keyCode);
+                            if (i !== inputs.length - 1) inputs[i + 1].focus(); event.preventDefault();
+                        }
+                    }
+                });
+            }
+        } OTPInput();
+
+
+    });
+
+    return (
+        <div style={{ backgroundColor: "azure" }}>
+            <h2>Enter your OTP</h2>
+            <p> For your security,we need to verify your identity.We sent a 5- digit code to<br /><span>A code has been sent to</span> <small>****@gmail.com</small>.Please enter it Below.</p>
+            <div class=" height-100 d-flex justify-content-center align-items-center">
+                <div class="position-relative"> <div class="card p-2 text-center">
+                    <div id="otp" class="inputs d-flex flex-row justify-content-center mt-2">
+                        <input class="m-2 text-center form-control rounded" type="text" id="first" maxlength="1" />
+                        <input class="m-2 text-center form-control rounded" type="text" id="second" maxlength="1" />
+                        <input class="m-2 text-center form-control rounded" type="text" id="third" maxlength="1" />
+                        <input class="m-2 text-center form-control rounded" type="text" id="fourth" maxlength="1" />
+                        <input class="m-2 text-center form-control rounded" type="text" id="fifth" maxlength="1" />
+                    </div>
+                    <div class="mt-4"> <button class="btn" onClick={() => navigate('/main')}>Back</button><button class="btn btn-danger px-4 validate">Verify</button>
+
+                    </div>
+
+                </div>
+                    <p>Did't receive the email ? Check  your spam filter for an<br /> email from ****@gmail.com</p>
+
+                </div>
             </div>
         </div>
     )
